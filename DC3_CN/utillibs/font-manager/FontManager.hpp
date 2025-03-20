fab2717bc30da47f8b0f140c6836615ca1b9baa8
cokkeijigen <itsukezigen@gmail.com>
2025-03-20 15:33:27 +0800

    Create FontManager.hpp

---
+++ DC3_CN/utillibs/font-manager/FontManager.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <FontManagerGUI.hpp>
+#include <unordered_map>
+
+namespace Utils {
+
+    class FontManager
+    {
+       
+        std::unique_ptr<FontManagerGUI> m_GUI;
+        std::unordered_map<int32_t, HFONT> m_Fonts{};
+
+    public:
+
+        const inline static int UseCharSet
+        {
+            ::GetACP() == 936 ? ANSI_CHARSET : 0x86
+        };
+
+        FontManager(){}
+
+        FontManager(HWND hWnd);
+
+        auto Init(HWND hWnd) -> FontManager&;
+
+        auto GetGBKFont(int32_t size) -> HFONT;
+
+        auto GetJISFont(int32_t size) -> HFONT;
+
+        auto GUI() -> FontManagerGUI*;
+
+        auto GUIUpdateDisplayState() -> FontManager&;
+
+        auto GUIChooseFont() -> FontManager&;
+    };
+}
